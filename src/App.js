9c3ff1048ae16d36c3bb92dda91807dea53175ae
Dostonbek <mardonovdoston69@gmail.com>
2022-05-01 08:41:44 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <select>
+          <option value="1">1</option>
+          <option value="1">2</option>
+        </select>
+        <select>
+          <option value="1">1</option>
+          <option value="1">2</option>
+        </select>
+      </div>
+      <div>
+        <textarea cols="50" row="8"></textarea>
+      </div>
+      <div>
+        <textarea cols="50" row="8"></textarea>
+      </div>
+      <div>
+        <textarea cols="50" row="8"></textarea>
+      </div>
     </div>
   );
 }
